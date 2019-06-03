1ebf29447a33d39762a32589299dc74770fd130d
yutsukushi <ykushibi@gmail.com>
2019-06-02 21:44:05 -0400

    functional buttons displays ten different gifs, the buttons display the same gifs that aren't related to the topics

--- assets/gif.js
+++ assets/gif.js
@@ -1,9 +1,12 @@
 
-var topics = ["Darth Vader", "Chewbacca", "Yoda", "Kylo Ren", "Darth Maul", "Princess Leia", "Obi-Wan Kenobi", "Luke Skywalker", "Han Solo", "Death Trooper", "Clone Commando"];
 
-var apiKey = "QglVYj0PZgIqAiVv9pVoi60ji9n617ui";
+// // button click function here
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=star+wars&limit=10&rating=G";
+var starWarsChar = $(this).attr("starWarsChar"); //when specific button is clicked, the function will run on that specific button.
+
+var topics = ["Darth Vader", "Chewbacca", "Yoda", "Kylo Ren", "Darth Maul", "Princess Leia", "Obi-Wan Kenobi", "Luke Skywalker", "Han Solo", "Death Trooper"];
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=QglVYj0PZgIqAiVv9pVoi60ji9n617ui&q=" + starWarsChar + "&limit=10&rating=G";
 
 $.ajax({
     url: queryURL,
@@ -12,36 +15,46 @@ $.ajax({
 
 .then(function(response) {
 
-    var gifResults = response.data.url;
-    var gifRatings = response.data.rating;
+    var gifResults = response.data;
 
     for (var i = 0; i < topics.length; i++) { //Your app should take the topics in this array and create buttons in your HTML.
 
-        var gifBtn = $('<button type="button" class="btn btn-primary" id="' + topics[i] + '">' + topics[i] + '</button>'); // Try using a loop that appends a button for each string in the array.
-
-        gifBtn.on("click", ); //need to add an on.click for dynamically created buttons
+        gifBtn = $('<button type="button" class="btn btn-primary starWarsChar="' + topics[i] + '">' + topics[i] + '</button>'); //dynamically creates a button for each string in arr.
         
-        $("#gif-buttons").append(gifBtn);
+        $("#gif-buttons").append(gifBtn); //displays the buttons to the gif-buttons ID
+
+    $("#gif-buttons").on("click", gifBtn , function(){
+
+        console.log("it work");
         
-    }
+        for (var j = 0; j < gifResults.length; j++) {
+        
+        var p = $("<p>").text("Rating: " + gifRatings); //creates p tag and applies text displaying the gif rating
+        
+        var gifDiv = $("<div>"); //creates div tag
+        var gifRatings = gifResults[j].rating; //JSON dot notation for the gif ratings
+        var gifImage = $("<img>"); //creates img tag
+    
+        gifImage.attr("src", gifResults[j].images.fixed_height.url); //applies src attribute to img tag with the JSON dot notation
+        
+        gifDiv.append(p); //appends the p variable value to div tag
+        gifDiv.append(gifImage); //appends img tag w src attribute to div tag
+          
+        $("#load-gifs-here").prepend(gifDiv); //prepends each gif to the load-gifs-here ID
 
-    function gifClickHandler() { // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-    // Under every gif, display its rating (PG, G, so on).
-    //    * This data is provided by the GIPHY API.
-    //    * Only once you get images displaying with button presses should you move on to the next step.
+        }
+    })
 
-    }
+}
+//     function gifBehavior() { // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
-    function gifBehavior() { // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+//     }
 
-    }
+//     function userInput() { // Add a form to your page that takes a value from a user input box and adds it to your `topics` array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
 
-    function userInput() { // Add a form to your page that takes a value from a user input box and adds it to your `topics` array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
-
-    }
+//     }
 
 });
-
 
 
 
